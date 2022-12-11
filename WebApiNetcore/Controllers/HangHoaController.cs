eb06ecc3a5f7f06586ab759323ff72200a2631d3
mastersn <32404400+mastersn@users.noreply.github.com>
2022-12-11 20:44:59 +0700

    hoc http get, post, put còn chưa thêm httpdelete

---
+++ WebApiNetcore/Controllers/HangHoaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiNetcore.Models;
+
+namespace WebApiNetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVm)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVm.TenHangHoa,
+                DonGia = hangHoaVm.DonGia
+            };
+            hangHoas.Add(hanghoa);
+
+            return Ok(new
+            {
+                Success = true,
+                Data = hanghoa
+            });
+        }
+    }
+
+
+}
---
+++ WebApiNetcore/Models/HangHoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNetcore.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+
+
+    }
+    public class HangHoa: HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+
+
+    }
+}
